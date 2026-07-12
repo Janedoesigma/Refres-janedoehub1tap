d54c31b3dd325c643e5ff06ffc7255be6c537090
Jane_doe_sigma <matheuscurcio23@gmail.com>
2026-07-12 12:39:10 -0300

    Update PizzagameOurEliotMonster.lua

--- Janedoehubwearedevs/PizzagameOurEliotMonster.lua
+++ Janedoehubwearedevs/PizzagameOurEliotMonster.lua
@@ -1,1 +1,11 @@
-print("hello")
+local SupportedIds = {
+    [16281075967] = true,
+    [5617640646] = true
+}
+
+if not SupportedIds[game.PlaceId] then
+    game:GetService("Players").LocalPlayer:Kick("Game not supported. Please join Pizza Game.")
+    return
+end
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Janedoesigma/Refres-janedoehub1tap/main/Janedoehubwearedevs/Minimaizepizzagamem.lua"))()
